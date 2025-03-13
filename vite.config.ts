032b0bb478b31dd869fe48fc437e14740e84ad1e
Ibrahim Mammadov <ibrahim.mammadov@workgenius.com>
2025-03-13 12:34:03 +0100

    Update vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle-clone/",
+  base: "https://zxyctn.github.io/wordle-clone/",
   plugins: [react()],
 });
